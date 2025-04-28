2cbbe9f190d20b3e586dfec44c356e2e84db71df
Sachin Kushawaha <54734235+sachin42@users.noreply.github.com>
2025-04-28 23:15:00 +0000

    Refactor command handling in WidgetTerminal and improve Wi-Fi status reporting

--- main/main.cpp
+++ main/main.cpp
@@ -6,7 +6,9 @@
 #include "wifi_connect.h"
 #include "WString.h"
 #include "config_b.h"
-// #define BLYNK_DEBUG
+#include "esp_wifi.h"
+
+//  #define BLYNK_DEBUG
 #define BLYNK_PRINT stdout
 #define BLYNK_TOKEN BLYNK_AUTH_TOKEN // Replace with your Blynk token
 #define BLYNK_SERVER BLYNK_CONFIG_SERVER
@@ -20,17 +22,28 @@ WidgetTerminal terminal(V0);
 BLYNK_WRITE(V0)
 {
   String receivedCommand = param.asString();
-     
-  terminal.print("Received Command: ");
-  terminal.println(receivedCommand);
+  receivedCommand.trim();
+  receivedCommand.toLowerCase();
 
   if (receivedCommand == "help")
   {
     terminal.println("Available Commands:");
-    terminal.println("- cpu: Get current cpu temperature");
     terminal.println("- status: Check device status");
     terminal.println("- restart: Restart the device");
     terminal.println("- clear: Clear the terminal");
+  }
+  else if (receivedCommand == "status")
+  {
+    wifi_ap_record_t ap_info;
+    if (esp_wifi_sta_get_ap_info(&ap_info) == ESP_OK)
+    {
+      terminal.printf( "Connected to SSID: %s\n", ap_info.ssid);
+      terminal.printf( "Signal strength (RSSI): %d dBm\n", ap_info.rssi);
+    }
+    else
+    {
+      ESP_LOGW("MAIN", "Wi-Fi not connected");
+    }
   }
   else if (receivedCommand == "restart")
   {
@@ -53,31 +66,31 @@ BLYNK_WRITE(V0)
 
 BLYNK_WRITE(V1)
 {
-    printf("Got a value: %s\n", param.asString());
+  printf("Got a value: %s\n", param.asString());
 }
 
 extern "C" void app_main(void)
 {
-    esp_log_level_set("wifi", ESP_LOG_ERROR);
-    esp_log_level_set("wifi_init", ESP_LOG_ERROR);
-    esp_log_level_set("phy_init", ESP_LOG_ERROR);
-    esp_log_level_set("wifi_prov_scheme_ble", ESP_LOG_ERROR);
-    esp_log_level_set("esp_netif_handlers", ESP_LOG_ERROR);
-    esp_log_level_set("gpio", ESP_LOG_ERROR);
-    wifi_begin();
+  esp_log_level_set("wifi", ESP_LOG_ERROR);
+  esp_log_level_set("wifi_init", ESP_LOG_ERROR);
+  esp_log_level_set("phy_init", ESP_LOG_ERROR);
+  esp_log_level_set("wifi_prov_scheme_ble", ESP_LOG_ERROR);
+  esp_log_level_set("esp_netif_handlers", ESP_LOG_ERROR);
+  esp_log_level_set("gpio", ESP_LOG_ERROR);
+  wifi_begin();
+  vTaskDelay(pdMS_TO_TICKS(10));
+
+  Blynk.begin(BLYNK_TOKEN, BLYNK_SERVER, 8080);
+
+  terminal.clear();
+  terminal.println("Hello from ESP32-IDF ");
+  terminal.println("This is a test message.");
+  terminal.flush();
+
+  while (true)
+  {
+    Blynk.run();
+    timer.run();
     vTaskDelay(pdMS_TO_TICKS(10));
-
-    Blynk.begin(BLYNK_TOKEN, BLYNK_SERVER, 8080);
-
-    terminal.clear();
-    terminal.println("Hello from ESP32-IDF ");
-    terminal.println("This is a test message.");
-    terminal.flush();
-
-    while (true)
-    {
-        Blynk.run();
-        timer.run();
-        vTaskDelay(pdMS_TO_TICKS(10));
-    }
+  }
 }
